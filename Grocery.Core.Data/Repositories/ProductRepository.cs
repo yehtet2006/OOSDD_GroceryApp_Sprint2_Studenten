30bc8dc4eb2f82443ee0907edb3c26c3fde71da3
Ye Htet Rohim <80707945+yehtet2006@users.noreply.github.com>
2025-09-17 18:17:59 +0200

    Ee klien wijziging om te kijken of de workflow start.

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -9,7 +9,7 @@ namespace Grocery.Core.Data.Repositories
         public ProductRepository()
         {
             products = [
-                new Product(1, "Melk", 300),
+                new Product(1, "Mek", 300),
                 new Product(2, "Kaas", 100),
                 new Product(3, "Brood", 400),
                 new Product(4, "Cornflakes", 0)];
